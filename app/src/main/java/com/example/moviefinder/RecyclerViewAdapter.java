7bac77326273ccfbe92c22bc09bce7d47b083457
Intrepid Panda <jerick999@gmail.com>
2019-03-28 15:06:41 -0600

    Create a recyclerview adapter class

---
+++ app/src/main/java/com/example/moviefinder/RecyclerViewAdapter.java
@@ -0,0 +1,7 @@
+package com.example.moviefinder;
+
+import android.support.v7.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+}
