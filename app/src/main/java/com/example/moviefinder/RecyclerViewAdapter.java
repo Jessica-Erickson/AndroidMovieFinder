46761e10d0a2fa7bceee024df07c566887111eb8
Intrepid Panda <jerick999@gmail.com>
2019-03-28 15:11:19 -0600

    Add class within adapter for viewholder

--- app/src/main/java/com/example/moviefinder/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/moviefinder/RecyclerViewAdapter.java
@@ -3,5 +3,7 @@ package com.example.moviefinder;
 import android.support.v7.widget.RecyclerView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
+    }
 }
