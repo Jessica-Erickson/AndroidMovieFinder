aaa7a2845b381c65f4f2789780455999e2fcc37b
Intrepid Panda <jerick999@gmail.com>
2019-03-28 15:25:19 -0600

    Override oncreateviewholder method to inflate a movie poster view

--- app/src/main/java/com/example/moviefinder/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/moviefinder/RecyclerViewAdapter.java
@@ -1,8 +1,24 @@
 package com.example.moviefinder;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        int listItemId = R.layout.movie_poster_view;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachImmediately = false;
+        View view = inflater.inflate(listItemId, viewGroup, shouldAttachImmediately);
+
+        return new ViewHolder(view);
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
 
     }
