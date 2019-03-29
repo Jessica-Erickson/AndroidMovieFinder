a3c07db998d17b3346bbf1474aa0c3240a33c155
Intrepid Panda <jerick999@gmail.com>
2019-03-29 13:59:36 -0600

    In oncreate method, instantiate a new gridlayoutmanager for the recyclerview

--- app/src/main/java/com/example/moviefinder/MainActivity.java
+++ app/src/main/java/com/example/moviefinder/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.moviefinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.tv_movies_recycler);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2, GridLayoutManager.VERTICAL, false);
     }
 }
