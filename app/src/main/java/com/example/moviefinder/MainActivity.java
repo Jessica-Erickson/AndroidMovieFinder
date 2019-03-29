c954e887d5fc6f2defb229aa0cd8a7386412da88
Intrepid Panda <jerick999@gmail.com>
2019-03-29 13:52:25 -0600

    Add private member variable to refer to recyclerview in the xml

--- app/src/main/java/com/example/moviefinder/MainActivity.java
+++ app/src/main/java/com/example/moviefinder/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.moviefinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
